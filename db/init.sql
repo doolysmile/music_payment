a54b6df7b622494252c6aebbbdcef653d6d8af8f
KCH <doolysmile1@naver.com>
2022-10-18 22:20:24 +0900

    feat : baseCode 작성

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+# 테스트 DB 생성
+DROP DATABASE IF EXISTS music_test;
+CREATE DATABASE music_test;
+USE music_test;
+
+# 개발 DB 생성
+DROP DATABASE IF EXISTS music_dev;
+CREATE DATABASE music_dev;
+USE music_dev;
---
+++ src/main/java/com/ll/exam/music/app/base/initData/InitDataBefore.java
@@ -0,0 +1,7 @@
+package com.ll.exam.music.app.base.initData;
+
+public interface InitDataBefore {
+    default void before() {
+
+    }
+}
